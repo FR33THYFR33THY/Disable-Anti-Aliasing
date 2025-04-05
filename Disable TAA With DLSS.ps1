881dd878461f6519300f3053bd1a08fe0e43143e
FR33THY <fr33thy@gmail.com>
2025-04-05 11:51:44 +1100

    Update Disable TAA With DLSS.ps1

--- Disable TAA With DLSS.ps1
+++ Disable TAA With DLSS.ps1
@@ -306,7 +306,7 @@ $MultilineComment = @"
       <ProfileSetting>
         <SettingNameInfo>Anisotropic filtering mode</SettingNameInfo>
         <SettingID>282245910</SettingID>
-        <SettingValue>1</SettingValue>
+        <SettingValue>0</SettingValue>
         <ValueType>Dword</ValueType>
       </ProfileSetting>
       <ProfileSetting>
@@ -566,7 +566,7 @@ $MultilineComment = @"
       <ProfileSetting>
         <SettingNameInfo>Anisotropic filtering mode</SettingNameInfo>
         <SettingID>282245910</SettingID>
-        <SettingValue>1</SettingValue>
+        <SettingValue>0</SettingValue>
         <ValueType>Dword</ValueType>
       </ProfileSetting>
       <ProfileSetting>
